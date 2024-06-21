ec1169b0ad1df996c82ae9e167e24d25aae2585b
OmarMoralesdev <matymv8288@gmail.com>
2024-06-21 14:14:37 -0600

    new bd

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,19 @@
 <?php
 $servername = "127.0.0.1";
 $username = "root";
-$password = "";
+$password = "12345678";
 $dbname = "TALLER_EURO";
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+try{
+$conexion = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password, );
 
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Conexión exitosa";
 }
-?>
+
+catch(PDOException $e){
+echo "Error: ". $e->getMessage();
+}
+$conexion=null;
+
