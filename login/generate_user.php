38a68cdd01908518358e94f4a5d22704f275bb45
OmarMoralesdev <matymv8288@gmail.com>
2024-06-27 12:15:42 -0600

    cambios registro

--- login/generate_user.php
+++ login/generate_user.php
@@ -1,5 +1,7 @@
 <?php
-require '../includes/db.php'; 
+
+require '../includes/db.php';
+
 
 function generateRandomPassword($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -11,45 +13,51 @@ function generateRandomPassword($length = 10) {
     return $randomPassword;
 }
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $nombre = trim($_POST['nombre']);
     $apellido_paterno = trim($_POST['apellido_paterno']);
     $apellido_materno = trim($_POST['apellido_materno']);
     $correo = trim($_POST['correo']);
     $telefono = trim($_POST['telefono']);
-    $password = generateRandomPassword();
     
 
+    $password = generateRandomPassword();
+
+
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $role = 'cliente'; 
-    $conn->beginTransaction();
-    
+
+    $role = 'cliente';
+
     try {
-      
-        $stmt_cliente = $conn->prepare("INSERT INTO CLIENTES (nombre, apellido_paterno, apellido_materno, correo, telefono) VALUES ( ?, ?, ?, ?, ?)");
+  
+        $stmt_cliente = $conn->prepare("INSERT INTO CLIENTES (nombre, apellido_paterno, apellido_materno, correo, telefono) VALUES (?, ?, ?, ?, ?)");
         $stmt_cliente->execute([$nombre, $apellido_paterno, $apellido_materno, $correo, $telefono]);
         
+       
         if ($stmt_cliente->rowCount() > 0) {
           
             $clienteID = $conn->lastInsertId();
             
-         
+           
             $stmt_usuario = $conn->prepare("INSERT INTO users (username, roles, password, clienteID) VALUES (?, ?, ?, ?)");
             $stmt_usuario->execute([$correo, $role, $hashed_password, $clienteID]);
             
+         
             if ($stmt_usuario->rowCount() > 0) {
-
-                $conn->commit();
+ 
                 echo "<div class='alert alert-success'>Usuario registrado exitosamente.<br>Nombre de usuario: <strong>$correo</strong><br>Contraseña: <strong>$password</strong></div>";
             } else {
-                throw new Exception('Error al insertar el usuario.');
+          
+                echo "<div class='alert alert-danger'>Error al insertar el usuario.</div>";
             }
         } else {
-            throw new Exception('Error al insertar el cliente.');
+            
+            echo "<div class='alert alert-danger'>Error al insertar el cliente.</div>";
         }
-    } catch (Exception $e) {
-     
-        $conn->rollback();
+    } catch (PDOException $e) {
+        
         echo "<div class='alert alert-danger'>Error: " . $e->getMessage() . "</div>";
     }
 }
