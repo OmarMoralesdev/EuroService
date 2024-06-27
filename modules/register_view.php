c9e8f8e228a85c41dce48b3d15e3ce4241d2014a
JorgeLIV <23170082@uttcampus.edu.mx>
2024-06-26 18:41:53 -0600

    orden

---
+++ modules/register_view.php
@@ -0,0 +1,47 @@
+
+<?php
+include '../includes/vabr.html';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTO CLIENTE</title>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h2>Registrar Cliente</h2>
+    <br>
+    <form method="post" action="generate_user.php">
+        <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" required>
+        </div>
+        <div class="form-group">
+            <label for="apellido_paterno">Apellido Paterno:</label>
+            <input type="text" class="form-control" id="apellido_paterno" name="apellido_paterno" required>
+        </div>
+        <div class="form-group">
+            <label for="apellido_materno">Apellido Materno:</label>
+            <input type="text" class="form-control" id="apellido_materno" name="apellido_materno" required>
+        </div>
+        <div class="form-group">
+            <label for="correo">Correo Electrónico:</label>
+            <input type="email" class="form-control" id="correo" name="correo" required>
+        </div>
+        <div class="form-group">
+            <label for="telefono">Teléfono:</label>
+            <input type="text" class="form-control" id="telefono" name="telefono" required>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-dark">Registrar</button>
+    </form>
+</div>
+</body>
+</html>
+
+
+
