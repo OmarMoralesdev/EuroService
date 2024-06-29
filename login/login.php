b4ccd94fd57f05184380e077171228909fe35c82
OmarMoralesdev <matymv8288@gmail.com>
2024-06-29 16:18:16 -0600

    login funcionando

--- login/login.php
+++ login/login.php
@@ -54,113 +54,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (PDOException $e) {
         echo "Error en la consulta: " . $e->getMessage();
     }
+    $valid_username = 'usuario';
+    $valid_password = 'contraseña';
 
+    if ($_POST['username'] === $valid_username && $_POST['password'] === $valid_password) {
+   
+        header('Location: login_view.php');
+        exit();
+    } else {
+ 
+        $_SESSION['error'] = 'Nombre de usuario o contraseña incorrectos.';
+        header('Location: login_view.php');
+        exit();
+    }
    
     $conn = null;
 }
 ?>
-=======
-        .form-container {
-            width: 100%;
-            max-width: 500px;
-            padding: 20px;
-            background-color: #A3A3A3;
-            border-radius: 10px;
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
-        }
 
-        .form-container h1 {
-            text-align: center;
-        }
-
-        .form-container h2 {
-            font-size: 30px;
-            margin-bottom: 20px;
-            text-align: left;
-        }
-
-        .form-group {
-            margin-bottom: 15px;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            border: 2px solid #100630;
-            border-radius: 5px;
-            background: linear-gradient(to right, #F0F0F0 25%, #C9C9C9 75%);
-            width: 100%;
-            padding: 10px;
-        }
-
-        input[type="text"]:focus,
-        input[type="password"]:focus {
-            outline: none;
-            box-shadow: 0 0 5px #1544FF;
-        }
-
-        input[type="submit"] {
-            background: linear-gradient(to right, #000000 25%, #171717 75%);
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            font-size: 16px;
-            cursor: pointer;
-            border-radius: 5px;
-            width: 100%;
-        }
-
-        input[type="submit"]:hover {
-            background: #383838;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-        }
-    </style>
-</head>
-
-<body>
-    <div class="form-container">
-        <h1>TALLER EURO SERVICE</h1>
-        <h2>Bienvenido</h2>
-        <form method="post" action="loginb.php">
-            <div class="form-group">
-                <label for="username">Nombre:</label>
-                <input type="text" class="form-control" id="username" name="username" autocomplete="on" placeholder="Ingresa tu usuario" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Contraseña:</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Ingresa tu contraseña" required>
-            </div>
-            <input type="submit" value="Iniciar sesión">
-        </form>
-    </div>
-    <?php
-    session_start();
-    if (isset($_SESSION['error']) && $_SESSION['error']) : ?>
-        <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="errorModalLabel">Error</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <?php echo htmlspecialchars($_SESSION['error'], ENT_QUOTES, 'UTF-8'); ?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <script>
-            $(document).ready(function() {
-                $('#errorModal').modal('show');
-            });
-        </script>
-        <?php unset($_SESSION['error']); ?>
-    <?php endif; ?>
-</body>
-
-</html>
->>>>>>> a580fdc06025389d0b87eede2a6711b5ad88aca2
