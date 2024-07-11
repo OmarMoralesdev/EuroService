e8c715a8edc017cf75f9d6909a087827a4fc7840
FH9CG <franciscohurtado336@gmail.com>
2024-07-11 15:42:39 -0600

    Parte 1 - ubicaciones

--- Euro service/vehiculos/ubicaciones_view.php
+++ Euro service/vehiculos/ubicaciones_view.php
@@ -11,12 +11,56 @@
         <div class="main p-3">
         <form action="#" method="post">
         <div class="mb-3">
-            <select class="form-select" name="ubi" id="ubi">
+            <select class="form-select" name="ubi" id="ubi" required>
             <option value="">Seleccione una ubicación</option>
+            <?php
+                    include '../includes/db.php';
+                    $conexion = new Database();
+                    $conexion->conectar();
+                    $consulta = "SELECT * FROM ubicaciones";
+                    $ubicaciones = $conexion->seleccionar($consulta);
+                    foreach($ubicaciones as $ubicacion) {
+                        echo "<option value='".$ubicacion->ubicacionID."'>".$ubicacion->lugar."</option>";
+                    }
+                    ?>
             </select>
         </div>
             <button type="submit" class="btn btn-primary" data-bs-toggle="modal">Buscar</button>
         </form>
+        <br>
+        <?php
+        extract($_POST);
+        if($_POST)
+        {
+            $consulta = "SELECT distinct concat(vehiculos.marca,' ',vehiculos.modelo,' ',vehiculos.anio,' ',vehiculos.color) AS VEHICULO, 
+concat(personas.nombre,' ',personas.apellido_paterno,' ',personas.apellido_materno) AS PROPIETARIO
+FROM personas INNER JOIN clientes ON clientes.personaID=personas.personaID INNER JOIN vehiculos ON
+vehiculos.clienteID=clientes.clienteID INNER JOIN citas ON citas.vehiculoID=vehiculos.vehiculoID INNER JOIN
+ordenes_trabajo ON ordenes_trabajo.citaID=citas.citaID INNER JOIN ubicaciones ON 
+ordenes_trabajo.ubicacionID=ubicaciones.ubicacionID 
+where ubicaciones.ubicacionID=$ubi";
+
+            $tabla = $conexion->seleccionar($consulta);
+
+            echo "
+            <table class='table table-hover'>
+            <thead class='table-dark' align='center'>
+            <tr>
+            <th>Vehículo</th><th>Propietario</th>
+            </tr>
+            </thead>
+            <tbody>";
+            foreach($tabla as $reg)
+            {
+            echo "<tr align='center'>";
+            echo "<td> $reg->VEHICULO </td>";
+            echo "<td> $reg->PROPIETARIO </td>";
+            echo "</tr>";
+            }
+            echo "</tbody>";
+
+        }
+        ?>
         </div>
     </div>
 </body>
