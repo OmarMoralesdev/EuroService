37abcf1cd077441f2326a10ee3b8051a63a3547b
OmarMoralesdev <matymv8288@gmail.com>
2024-07-12 17:08:34 -0600

    html5

--- Euro service/vehiculos/autos_view.php
+++ Euro service/vehiculos/autos_view.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Vehículos</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body>
+    
 <div class="wrapper">
         <?php include '../includes/vabr.html'; ?>
         <div class="main p-3">
