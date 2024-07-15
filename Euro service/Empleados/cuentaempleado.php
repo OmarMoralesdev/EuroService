0af295e4574053f14f78a38489d51db922bc8b6b
OmarMoralesdev <matymv8288@gmail.com>
2024-07-15 11:47:29 -0600

    empelados jalando

--- Euro service/Empleados/cuentaempleado.php
+++ Euro service/Empleados/cuentaempleado.php
@@ -1,51 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <title>Registro de Cuenta de Administrador</title>
 </head>
+
 <body>
-<h2>Registro de Cuenta de Administrador</h2>
-    <form action="registrocuena.php" method="POST">
-    <div class="mb-3">
-            <label for="empleado" class="form-label">Empleado ID:</label>
-            <select name="empleado" class="form-control" required>
-                <?php
-                require '../includes/db.php';
-                $con = new Database();
-                $pdo = $con->conectar();
-                function obtenerEmpleadosDisponibles($pdo)
-                {
-                    $sql = "SELECT EMPLEADOS.empleadoID, PERSONAS.nombre, PERSONAS.apellido_paterno, PERSONAS.apellido_materno 
+    <div class="wrapper">
+        <?php include '../includes/vabr.html'; ?>
+        <div class="main p-3">
+            <h2>Registro de Cuenta de Administrador</h2>
+            <form action="registrocuena.php" method="POST">
+
+                <label for="empleado" class="form-label">Empleado:</label>
+                <select name="empleado" class="form-control" required>
+                    <?php
+                    require '../includes/db.php';
+                    $con = new Database();
+                    $pdo = $con->conectar();
+                    function obtenerEmpleadosDisponibles($pdo)
+                    {
+                        $sql = "SELECT EMPLEADOS.empleadoID, PERSONAS.nombre, PERSONAS.apellido_paterno, PERSONAS.apellido_materno 
                                         FROM EMPLEADOS 
                                         JOIN PERSONAS ON EMPLEADOS.personaID = PERSONAS.personaID
                                         where EMPLEADOS.tipo = 'administrativo'";
-                    $stmt = $pdo->query($sql);
-                    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-                }
-                $empleados = obtenerEmpleadosDisponibles($pdo);
-                foreach ($empleados as $empleado) {
-                    $nombreCompleto = "{$empleado['nombre']} {$empleado['apellido_paterno']} {$empleado['apellido_materno']}";
-                    echo "<option value=\"{$empleado['empleadoID']}\">{$nombreCompleto}</option>";
-                }
-                ?>
-            </select>
-            <div class="invalid-feedback">Debes seleccionar un empleado.</div>
-        </div>
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br><br>
-        
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
-        
-        <label for="confirm_password">Confirmar Password:</label><br>
-        <input type="password" id="confirm_password" name="confirm_password" required><br><br>
-        
-        
-        <input type="submit" value="Registrar">
+                        $stmt = $pdo->query($sql);
+                        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    }
+                    $empleados = obtenerEmpleadosDisponibles($pdo);
+                    foreach ($empleados as $empleado) {
+                        $nombreCompleto = "{$empleado['nombre']} {$empleado['apellido_paterno']} {$empleado['apellido_materno']}";
+                        echo "<option value=\"{$empleado['empleadoID']}\">{$nombreCompleto}</option>";
+                    }
+                    ?>
+                </select>
+                <div class="invalid-feedback">Debes seleccionar un empleado.</div>
 
-    </form>
+                <label for="username">Username:</label><br>
+                <input type="text" id="username" name="username" class="form-control" required><br><br>
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" class="form-control" required><br><br>
+                <label for="confirm_password">Confirmar Password:</label><br>
+                <input type="password" id="confirm_password" name="confirm_password" class="form-control" required><br><br>
+                <input type="submit" value="Registrar">
+            </form>
+        </div>
+    </div>
+
 </body>
+
 </html>
--- Euro service/Empleados/registro_empleado.php
+++ Euro service/Empleados/registro_empleado.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REGISTRO CLIENTE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <style>
         .form-group {
             margin-bottom: 5px;
@@ -17,8 +18,7 @@
     <div class="wrapper">
         <?php include '../includes/vabr.html'; ?>
         <div class="main p-3">
-            <div class="container"
-                style="width: 90%; margin: auto; background-color: #EBEBEB; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
+            <div class="container" style="width: 90%; margin: auto; background-color: #EBEBEB; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
                 <h2>Registrar Cliente</h2>
                 <br>
                 <form method="post" action="registrar_empleado.php">
@@ -35,15 +35,15 @@
                         <input type="text" class="form-control" id="apellido_materno" name="apellido_materno" required>
                     </div>
                     <label for="nombre">Alias:</label>
-                    <input type="text" id="nombre" name="alias" required><br><br>
+                    <input type="text" id="nombre" name="alias" class="form-control" required><br><br>
                     <label for="tipo">Tipo:</label>
-                    <select id="tipo" name="tipo" required>
+                    <select id="tipo" name="tipo" class="form-control" required>
                         <option value="master">Master</option>
                         <option value="intermedio">Intermedio</option>
                         <option value="ayudante">Ayudante</option>
                     </select><br><br>
                     <br>
-                    <button type="submit" class="btn btn-dark w-100">Registrar</button>
+                    <button type="submit" class="btn btn-dark w-100 ">Registrar</button>
                 </form>
             </div>
         </div>
@@ -51,5 +51,3 @@
 </body>
 
 </html>
-
- 
