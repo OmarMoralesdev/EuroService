98acecd070532f92e457be1363486aaf64cd7d70
JorgeLIV <23170082@uttcampus.edu.mx>
2024-07-15 13:12:51 -0600

    x

--- Euro service/vehiculos/autos.php
+++ Euro service/vehiculos/autos.php
@@ -3,29 +3,33 @@ require '../includes/db.php';
 $con = new Database();
 $pdo = $con->conectar();
 
-$nombre = $_POST['nombre'];
-$marca = $_POST['marca'];
-$modelo = $_POST['modelo'];
-$año = $_POST['año'];
-$color = $_POST['color'];
-$placas = $_POST['placas'];
-$vin = $_POST['vin'];
 
-$verificar = "SELECT * FROM VEHICULOS WHERE vin = ?";
-$stmtVerificar = $pdo->prepare($verificar);
-$stmtVerificar->execute([$vin]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $clienteID = $_POST['clienteID'];
+    echo "Selected Client ID: $clienteID";
+    $marca = trim($_POST['marca']);
+    $modelo = trim($_POST['modelo']);
+    $anio = trim($_POST['anio']);
+    $color = trim($_POST['color']);
+    $kilometraje = trim($_POST['kilometraje']);
+    $placas = trim($_POST['placas']);
+    $vin = trim($_POST['vin']);
 
-if ($stmtVerificar->rowCount() > 0) {
-    echo "El vehículo ya está registrado.";
-} else {
-    $sql = "INSERT INTO VEHICULOS (nombre, marca, modelo,año,color,placas,vin) VALUES (?, ?, ?,?,?, ?, ?)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$nombre, $marca, $modelo,$año,$color,$placas,$vin]);
+    $verificar = "SELECT * FROM VEHICULOS WHERE vin = ?";
+    $stmtVerificar = $pdo->prepare($verificar);
+    $stmtVerificar->execute([$vin]);
 
-    if ($stmt->rowCount() > 0) {
-        echo "Vehículo registrado exitosamente.";
+    if ($stmtVerificar->rowCount() > 0) {
+        echo "El vehículo ya está registrado.";
     } else {
-        echo "Error: " . $sql . "<br>" . $pdo->errorInfo()[2];
+        $sql = "INSERT INTO VEHICULOS (clienteID, marca, modelo,anio,color,kilometraje,placas,vin) VALUES (?, ?, ?,?,?, ?, ?,?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$clienteID, $marca, $modelo, $anio, $color, $kilometraje, $placas, $vin]);
+
+        if ($stmt->rowCount() > 0) {
+            echo "Vehículo registrado exitosamente.";
+        } else {
+            echo "Error: " . $sql . "<br>" . $pdo->errorInfo()[2];
+        }
     }
 }
-?>
