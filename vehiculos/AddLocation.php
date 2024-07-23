31be9e9985d7087d6173019763141d72d559d8d8
FH9CG <franciscohurtado336@gmail.com>
2024-07-23 15:08:00 -0600

    Faltan los modal

--- vehiculos/AddLocation.php
+++ vehiculos/AddLocation.php
@@ -9,29 +9,74 @@ $modalContent = '';
 
     $conexion = new Database();
     $conexion->conectar();
+    $conn = $conexion->conectar();
 
-    $consulta_nombre = "SELECT COUNT(*) as count FROM UBICACIONES WHERE lugar = '$lugar'";
-    $resultado_nombre = $conexion->ejecuta($consulta_nombre);
-    
+    $consulta_nombre = "SELECT COUNT(*) as count FROM ubicaciones WHERE lugar = :lugar";
+    $stmt_nombre = $conn->prepare($consulta_nombre);
+    $stmt_nombre->bindParam(':lugar', $lugar, PDO::PARAM_STR);
+    $stmt_nombre->execute();
+    $resultado_nombre = $stmt_nombre->fetch(PDO::FETCH_ASSOC);
 
     
-    
-    if($capacidad > 0 && $capacidad <= 40 ){
+    if ($resultado_nombre['count'] > 0) {
+        echo "<!-- Modal de Error -->
+<div class='modal fade' id='errorModal' tabindex='-1' aria-labelledby='errorModalLabel' aria-hidden='true'>
+    <div class='modal-dialog'>
+        <div class='modal-content'>
+            <div class='modal-header'>
+                <h5 class='modal-title' id='errorModalLabel'>Error al agregar ubicación</h5>
+                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            </div>
+            <div class='modal-body'>
+                <p>Ya existe una ubicación con el nombre '$lugar'.</p>
+            </div>
+            <div class='modal-footer'>
+                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>";
+    } else {
+        if($capacidad > 0 && $capacidad <= 40 ){
 
-        $consulta = "INSERT INTO ubicaciones (lugar, capacidad, activo) VALUES ('$lugar', $capacidad, 'si')";
-        $conexion->ejecuta($consulta);
-        
-        $conexion->desconectar();
-        
-        header('Location: ubicaciones_view.php');
-        exit();
-    }else if ($capacidad < 0){
+            $consulta = "INSERT INTO ubicaciones (lugar, capacidad, activo) VALUES ('$lugar', $capacidad, 'si')";
+            $conexion->ejecuta($consulta);
+            
+            $conexion->desconectar();
+            
+            header('Location: ubicaciones_view.php');
+            exit();
+        }else if ($capacidad < 0){
+            echo "
+                <div class='modal fade' id='staticBackdrop' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
+                    <div class='modal-dialog'>
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <h1 class='modal-title fs-5' id='staticBackdropLabel'>No puedes ingresar números negativos!</h1>
+                                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                            </div>
+                            <div class='modal-body'>
+                                Ingresa datos válidos.<br><br>
+                                
+                            </div>
+                            <div class='modal-footer'>
+                                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+                header('Location: ubicaciones_view.php');
+                exit();
+            
+    
+        }
+        else if ($capacidad > 40){
         $modalContent = "
             <div class='modal fade' id='staticBackdrop' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
                 <div class='modal-dialog'>
                     <div class='modal-content'>
                         <div class='modal-header'>
-                            <h1 class='modal-title fs-5' id='staticBackdropLabel'>No puedes ingresar números negativos!</h1>
+                            <h1 class='modal-title fs-5' id='staticBackdropLabel'>La capacidad no puede ser mayor a 40</h1>
                             <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
                         </div>
                         <div class='modal-body'>
@@ -46,31 +91,8 @@ $modalContent = '';
             </div>";
             header('Location: ubicaciones_view.php');
             exit();
-        
-
+    
     }
-    else if ($capacidad > 40){
-    $modalContent = "
-        <div class='modal fade' id='staticBackdrop' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-                <div class='modal-content'>
-                    <div class='modal-header'>
-                        <h1 class='modal-title fs-5' id='staticBackdropLabel'>La capacidad no puede ser mayor a 40</h1>
-                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                    </div>
-                    <div class='modal-body'>
-                        Ingresa datos válidos.<br><br>
-                        
-                    </div>
-                    <div class='modal-footer'>
-                        <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
-                    </div>
-                </div>
-            </div>
-        </div>";
-        header('Location: ubicaciones_view.php');
-        exit();
-
-}
+    }
 }
 ?>
