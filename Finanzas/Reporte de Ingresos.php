d7b0bd692ea9f548ca9e0a596976241424f0410e
JorgeLIV <23170082@uttcampus.edu.mx>
2024-07-23 16:31:52 -0600

    x

--- Finanzas/Reporte de Ingresos.php
+++ Finanzas/Reporte de Ingresos.php
@@ -44,16 +44,6 @@ try {
 <head>
     <meta charset="UTF-8">
     <title>Reporte de Ingresos Semanal</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body {
-            padding-top: 20px;
-        }
-
-        .container {
-            max-width: 900px;
-        }
-    </style>
 </head>
 
 <body>
@@ -100,9 +90,6 @@ try {
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
