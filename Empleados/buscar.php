181cb0b0eedd81196b0421ef354b11a5361113b9
JorgeLIV <23170082@uttcampus.edu.mx>
2024-07-25 18:27:49 -0600

    chore: Eliminar archivos innecesarios y mejorar búsqueda de empleados

--- Empleados/buscar.php
+++ Empleados/buscar.php
@@ -47,7 +47,7 @@ $empleados = $conexion->seleccionar($consulta_empleados);
         <?php include '../includes/vabr.html'; ?>
         <div class="main p-3">
             <div class="container">
-                <h2 class="text-center">CONTROL FINANCIERO EMPLEADOS</h2>
+                <h2 class="text-center">EMPLEADOS</h2>
                 <div class="form-container">
                     <!-- Formulario de búsqueda -->
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="mb-3">
@@ -71,39 +71,11 @@ $empleados = $conexion->seleccionar($consulta_empleados);
                                 echo "<p class='card-text'><strong>Tipo:</strong> {$empleado->tipo}</p>";
                                 echo "<p class='card-text'><strong>Salario:</strong> {$empleado->total}</p>";
 
-                                // Botones para agregar deudas y rebajas
-                                echo "<button type='button' class='btn btn-dark btn-md ml-2'  style='width: 49% ;' data-bs-toggle='modal' data-bs-target='#modalRebajas{$empleado->empleadoID}'>REBAJAS</button>";
-                                echo" ";
-                                echo "<button type='button' class='btn btn-dark btn-md ml-2'  style='width: 49% ;' data-bs-toggle='modal' data-bs-target='#modalBonos{$empleado->empleadoID}'>BONOS</button>";
-                                echo "</div>";
+                                echo "<button type='button' class='btn btn-dark btn-md ml-2'  style='width: 100% ;' data-bs-toggle='modal' data-bs-target='#modalRebajas{$empleado->empleadoID}'>REBAJAS</button>";
                                 echo "</div>";
                                 echo "</div>";
                                 echo "</div>";
 
-                                // Modal para agregar bonos
-                                echo "<div class='modal fade' id='modalBonos{$empleado->empleadoID}' tabindex='-1' aria-labelledby='modalBonos{$empleado->empleadoID}Label' aria-hidden='true'>";
-                                echo "<div class='modal-dialog'>";
-                                echo "<div class='modal-content'>";
-                                echo "<div class='modal-header'>";
-                                echo "<h5 class='modal-title' id='modalBonos{$empleado->empleadoID}Label'>Agregar Bonos para {$empleado->nombre_completo}</h5>";
-                                echo "<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>";
-                                echo "</div>";
-                                echo "<div class='modal-body'>";
-                                echo "<form>";
-                                echo "<div class='mb-3'>";
-                                echo "<label for='bono{$empleado->empleadoID}'>Cantidad del bono:</label>";
-                                echo "<input type='number' class='form-control' id='bono{$empleado->empleadoID}' name='bono'>";
-                                echo "</div>";
-                                echo "</form>";
-                                echo "</div>";
-                                echo "<div class='modal-footer'>";
-                                echo "<button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>";
-                                echo "<button type='button' class='btn btn-dark'>Guardar</button>";
-                                echo "</div>";
-                                echo "</div>";
-                                echo "</div>";
-                                echo "</div>";
-                            
                                 // Modal para agregar rebajas
                                 echo "<div class='modal fade' id='modalRebajas{$empleado->empleadoID}' tabindex='-1' aria-labelledby='modalRebajas{$empleado->empleadoID}Label' aria-hidden='true'>";
                                 echo "<div class='modal-dialog'>";
