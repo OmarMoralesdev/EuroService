484c02cfaa1dfc82ebd326fcd9924ca2e1cde976
OmarMoralesdev <matymv8288@gmail.com>
2024-08-07 22:52:40 -0600

    redireccionamiento login

--- includes/vabr.php
+++ includes/vabr.php
@@ -1,3 +1,12 @@
+<?php
+//session_start();
+
+//if (!isset($_SESSION['username']) || !isset($_SESSION['empleadoID'])) {
+   // header('Location: ../Login/index.php');
+    //exit();
+//}
+?>
+
 <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
