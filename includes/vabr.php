eb4985434e11528d488b0092963d9fc2f6f039bc
OmarMoralesdev <matymv8288@gmail.com>
2024-08-09 01:30:28 -0600

    icono

--- includes/vabr.php
+++ includes/vabr.php
@@ -5,7 +5,7 @@
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
+<link rel="icon" type="image/x-icon" href="../img/icon.svg">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <style>
