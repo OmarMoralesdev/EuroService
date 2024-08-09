2efff8aa9dc47880e7986b12c27ffa2ab3d52dcf
JorgeLIV <23170082@uttcampus.edu.mx>
2024-08-09 16:48:17 -0600

    xd

--- Finanzas/gestion_pagoforms.php
+++ Finanzas/gestion_pagoforms.php
@@ -91,12 +91,12 @@ try {
                                         $estado_pago = $fila['estado_pago'];
                                         $color_celda_estado = "";
 
-                                        if ($estado_pago == '#00df18') {
-                                            $color_celda_estado = "#a4fa83"; // Verde claro
-                                        } elseif ($estado_pago == '#ffc500') {
-                                            $color_celda_estado = "#ffdf71"; // Naranja claro
-                                        } elseif ($estado_pago == '#df0000') {
-                                            $color_celda_estado = "#e86161"; // Rojo claro
+                                        if ($estado_pago == "#42ec00") {
+                                            $color_celda_estado = "#d4edda"; // Verde claro
+                                        } elseif ($estado_pago == "#ec9300") {
+                                            $color_celda_estado = "#fff3cd"; // Naranja claro
+                                        } elseif ($estado_pago == "#d4edda") {
+                                            $color_celda_estado = "#ec0000"; // Rojo claro
                                         }
                                         ?>
                                         <tr>
