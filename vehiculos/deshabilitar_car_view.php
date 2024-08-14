53ce12b1397052d84678bf886584bb2c5302759d
FH9CG <franciscohurtado336@gmail.com>
2024-08-13 22:28:06 -0600

    Habilitar vehículo

--- vehiculos/deshabilitar_car_view.php
+++ vehiculos/deshabilitar_car_view.php
@@ -30,6 +30,26 @@
     <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#enableCarModal">HABILITAR VEHÍCULO</button>
 </div>
                 <?php
+                                    if (isset($_SESSION['x'])) {
+                                        echo "
+                                        <div class='modal fade' id='staticBackdrop' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
+                                            <div class='modal-dialog'>
+                                                <div class='modal-content'>
+                                                    <div class='modal-header'>
+                                                        <h1 class='modal-title fs-5' id='staticBackdropLabel'>Vehículo Habilitado!</h1>
+                                                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                                                    </div>
+                                                    <div class='modal-body'>
+                                                        <div class='alert alert-success' role='alert'>{$_SESSION['x']}</div>
+                                                    </div>
+                                                    <div class='modal-footer'>
+                                                        <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>";
+                                        unset($_SESSION['x']);
+                                    }
                     if (isset($_SESSION['error'])) {
                         echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error'] . '</div>';
                         unset($_SESSION['error']); // Limpiar el mensaje después de mostrarlo
@@ -52,8 +72,8 @@
                                 </div>
                             </div>
                         </div>";
+                        unset($_SESSION['bien']);
                     }
-                    unset($_SESSION['bien']);
                     ?>
             <div class="row mt-3">
             
--- vehiculos/enableCar.php
+++ vehiculos/enableCar.php
@@ -1,12 +1,25 @@
 <?php
+session_start();
 include '../includes/db.php';
-    $conexion = new Database();
-    $conexion->conectar();
-    extract($_POST);
-    $consulta = "UPDATE `VEHICULOS` SET `activo` = 'si' WHERE `vehiculoID` = $auto";
-    $conexion->ejecuta($consulta);
-    
-    $conexion->desconectar();
-    header('Location: deshabilitar_car_view.php');
-    exit();
+
+$conexion = new Database();
+$pdo = $conexion->conectar();
+
+extract($_POST);
+
+$sqlVehiculo = "SELECT CONCAT(marca, ' ', modelo, ' ', anio, ' ', color) AS VEHICULO
+FROM VEHICULOS
+WHERE vehiculoID = :vehiculoID";
+$queryVehiculo = $pdo->prepare($sqlVehiculo);
+$queryVehiculo->bindParam(':vehiculoID', $auto, PDO::PARAM_INT);
+$queryVehiculo->execute();
+$rowVehiculo = $queryVehiculo->fetch(PDO::FETCH_ASSOC);
+$nombre_Vehiculo = $rowVehiculo['VEHICULO'];
+$_SESSION['x'] = "El vehículo '$nombre_Vehiculo' se ha habilitado";
+
+$consulta = "UPDATE `VEHICULOS` SET `activo` = 'si' WHERE `vehiculoID` = $auto";
+$conexion->ejecuta($consulta);
+$conexion->desconectar();
+header('Location: deshabilitar_car_view.php');
+exit();
 ?>
